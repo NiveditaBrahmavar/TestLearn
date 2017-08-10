bb4c948ab43a88a8c0da655d76181f674112a40a
innbrahmav <innbrahmav@HIBAMP51552>
2017-08-10 12:25:04 +0530

    Add project files.

---
+++ WebApplicationTest/Controllers/LearnController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class LearnController : Controller
+    {
+        // GET: Learn
+        public ActionResult Index()
+        {
+             
+
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetSuggestion(string text)
+        {
+            List<MyShop> shpList = new List<MyShop>();
+            shpList.Add(new MyShop { ItemID = 1, ItemName = "Name1", IsAvailable = true });
+            shpList.Add(new MyShop { ItemID = 2, ItemName = "Name2", IsAvailable = true });
+            shpList.Add(new MyShop { ItemID = 3, ItemName = "Name3", IsAvailable = true });
+
+            List<string> newlist = new List<string>();
+
+            newlist = shpList.Where(x => x.ItemName.ToLower().Contains(text.ToLower())).Select(y=> y.ItemName).ToList();
+
+            return Json(newlist, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpGet]
+        public ActionResult ValidationCheck()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ValidationCheck(Customer cust)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            return View(cust);
+        }
+    }
+}
---
+++ WebApplicationTest/Models/Customer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace WebApplicationTest.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Enter Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Address")]
+        [MaxWordAttributes(10, ErrorMessage = "There are too many words in {0}.")]
+        // [MaxWordAttributes(10, ErrorMessage = "There are too many words in {0}.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter Age")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Select gender")]
+        public string Gender { get; set; }
+        
+        public bool IsGenderSelected { get; set; }
+
+        //[Required(ErrorMessage = "Enter other Details")]
+        [RequiredIf("Gender", ErrorMessage = "Enter other Details")]
+        //[RequiredIf("UserType", UserType.Admin, ErrorMessageResourceName = "PasswordRequired", ErrorMessageResourceType = typeof(ResourceString))]
+        public string OtherDetails { get; set; }
+    }
+    
+    public class MaxWordAttributes : ValidationAttribute
+    {
+        private readonly int _maxwords;
+        public MaxWordAttributes(int maxwords): base("{0} has too many words")
+        {
+            _maxwords = maxwords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null) return ValidationResult.Success;
+            var textvalue = value.ToString();
+            if (textvalue.Length <= _maxwords) return ValidationResult.Success;
+            var errmessage = FormatErrorMessage(validationContext.DisplayName);
+            return new ValidationResult(errmessage);
+        }
+    }
+
+    public class RequiredIfAttribute: ValidationAttribute
+    {
+        private string _propertyname;
+        private object _propertyvalue;
+       // private string _property;
+        public RequiredIfAttribute(string propertyname)
+        {
+            _propertyname = propertyname;
+         //   _propertyvalue = propertyvalue;
+        //    _property = property;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            PropertyInfo otherPropertyInfo = validationContext.ObjectType.GetProperty(_propertyname);
+            if (otherPropertyInfo == null)
+            {
+                return new ValidationResult($"Unknown property {_propertyname}");
+            }
+
+            object othervalue = otherPropertyInfo.GetValue(validationContext.ObjectInstance, null);
+            if (othervalue.ToString() == "F")
+            { // if other property has the configured value
+              // PropertyInfo prop = validationContext.ObjectType.GetProperty(_propertyname);
+              //ChangeableRequired attrib = (ChangeableRequired)prop.Attributes;
+
+                return ValidationResult.Success;
+
+            }
+
+            var errmessage = FormatErrorMessage(validationContext.DisplayName);
+            return new ValidationResult(errmessage);
+
+            // return null;
+            // return base.IsValid(value, validationContext);
+        }
+    }
+
+    public class ChangeableRequired : RequiredAttribute
+    {
+        public bool Disabled { get; set; }
+
+        public override bool IsValid(object value)
+        {
+            if (Disabled)
+            {
+                return true;
+            }
+
+            return base.IsValid(value);
+        }
+    }
+
+
+
+
+}
